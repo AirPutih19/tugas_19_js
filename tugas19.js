c2ea08927ae9171a2e99dccd6f9149831a1a02d3
AirPutih19 <kharismadwisj@gmail.com>
2023-05-16 15:54:33 +0700

    Tugas 19 JS
    Tugas 19 JS untuk niomic

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+console.log("Semangat...".repeat(5));
+console.log("");
+
+function ripit(){
+    var kata = "*****";
+    console.log(kata);
+    console.log(kata.repeat(5));
+    console.log(kata.repeat(10));
+    console.log(kata.repeat(15));
+    console.log(kata.repeat(20));
+    console.log(kata.repeat(15));
+    console.log(kata.repeat(10));
+    console.log(kata.repeat(5));
+    console.log(kata);
+    
+}
+ripit()
